989f6d386c8d3a79ca49f4a1cf9783d2af4c35d8
Fan Minghui <fmh_33@foxmail.com>
2016-10-10 14:17:58 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+  // 项目配置
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%=pkg.file %>.js',
+        dest: 'dest/<%= pkg.file %>.min.js'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+}
